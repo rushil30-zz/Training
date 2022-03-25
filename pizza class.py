d5cad6d29e2f6dbd4f22e0c118398dc5e072435c
Rushil Agarwal <rushil.agarwal@programmers.io>
2022-03-25 18:18:10 +0530

    Added Pizza Task

---
+++ pizza class.py
@@ -0,0 +1,20 @@
+class Pizza:
+
+    def add_topping(self,veggie, cheese, protein):
+        self.veggie = veggie
+        self.cheese = cheese
+        self.protein = protein
+        print(f"Your pizza has a topping of {veggie}, {cheese} and {protein}")
+        return
+
+pizza = Pizza()
+
+a = input("Enter a Veggie of ur choice:")
+b = input("Enter a cheese of your choice:")
+c = input("Enter a protein of your choice:")
+
+my_custom_pizza = pizza.add_topping(a, b, c)
+
+
+        
+        
