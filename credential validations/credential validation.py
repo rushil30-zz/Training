9d87750da0365b70386895476c467f3fa9a1c7ef
Rushil Agarwal <rushil.agarwal@programmers.io>
2022-03-30 16:24:34 +0530

    Added folder

--- credential validations/credential validation.py
+++ credential validations/credential validation.py
@@ -1,12 +1,23 @@
+import os
+from dotenv import load_dotenv
 import psycopg2
-from flask import Flask, make_response, request,jsonify
+from flask import Flask, make_response, request, jsonify
 from flask_cors import CORS
 import re
 
 app = Flask(__name__)
 cors = CORS(app)
-conn = psycopg2.connect(database = "postgres", user = "postgres", password = "testpwd", host = "127.0.0.1", port = "5432")
+load_dotenv()
+database = os.getenv('DATABASE')
+user = os.getenv('USER')
+password = os.getenv('PASSWORD')
+host = os.getenv('HOST')
+port = os.getenv('PORT')
+
+conn = psycopg2.connect(database = database, user = user, password = password, host = host, port = port)
 cur = conn.cursor()
+cur.execute("select * from credentials")
+data = cur.fetchall()
 
 
 def userdict(data):
@@ -22,33 +33,34 @@ def userdict(data):
 
 
 @app.route("/user", methods=['POST','GET','DELETE','PATCH'])
-# function to add and validate new user
-def add_user():
+def user():
+    # to add and validate new user
     if request.method == 'POST':
         email_pattern = '^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$'
-        password_pattern = '^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$'
+        password_pattern = '^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$'
         req = request.get_json()
         email = req['email']
-        password = req['password']
         username = req['username']
+        password = req['password']
         email_result = re.match(email_pattern, email)
         password_result = re.match(password_pattern, password)
 
-        cur.execute("select username, email from credentials where username='{}' and email='{}'".format(username, email))
+        cur.execute("select username, email, password from credentials where username='{}' or email='{}' or password='{}'".format(username, email, password))
         rows = cur.fetchone()
         if rows == None:
             if (email_result and password_result):
                 cur.execute("insert into credentials (email, password, username) values (%s, %s, %s)", (email, password, username))
                 conn.commit()
-                res = make_response("User Added", 200)
+                res = make_response("User Added", 201)
                 return res
             else:
                 res = make_response("Invalid Email Address or Password", 400)
                 return res
         else:
-            res = make_response("Username or Email Already", 400)
-            return res      
+            res = make_response("Credentials already exists", 400)
+            return res   
 
+# to get all users
     if request.method == 'GET':
         cur.execute("select * from credentials")
         rows = cur.fetchall()
@@ -56,16 +68,8 @@ def add_user():
         res = make_response(jsonify(data), 200)
         return res
 
-
-
-@app.route("/user", methods=['GET'])
-# function to get all users
-
-   
-
-@app.route("/user", methods = ['DELETE'])
-# function to delete user
-def delete_user():
+# to delete a user
+    if request.method == 'DELETE': 
         req = request.get_json()
         email = req['email']
         password = req['password']
@@ -79,33 +83,32 @@ def delete_user():
             return res
         else:
             res = make_response("Invalid Credentials", 400)
-            return res
-        
+            return res  
 
-@app.route("/user", methods = ['PATCH'])
-# function to update password
-def change_password():
-    req = request.get_json()
+# to update password
+    if request.method =='PATCH':
+        req = request.get_json()
     username = req['username']
     password = req['password']
     new_password = req['new_password']
-    cur.execute("select * from credentials where password='{0}' and username='{1}'".format(password, username))
+    # cur.execute("select * from credentials where password='{0}' and username='{1}'".format(password, username))
+    cur.execute("select id from credentials where password='{0}' and username='{1}'".format(password, username))
     rows = cur.fetchone()
     if rows !=None:
         new_password == password
-        cur.execute("update credentials set password='{0}' where username='{1}'".format(new_password, username))
+        #cur.execute("update credentials set password='{0}' where username='{1}'".format(new_password, username))
+        cur.execute("update credentials set password='{0}' where id='{1}'".format(new_password, rows[0]))
         conn.commit()
         res = make_response("Password Updated", 200)
         return res
-
     else:
         res = make_response("Invalid Credentials", 400)
-        return res     
+        return res      
          
 
 if __name__ == "__main__":
     app.run(debug = True)
-    
+
 
 
 
